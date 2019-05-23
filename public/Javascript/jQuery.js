5013a7be548ed8d910001b295ef3ce43faf0279b
Angelo Otero <otero.angelo20@gmail.com>
2019-05-23 18:07:36 -0400

    Finish responsive behavior for now

--- public/Javascript/jQuery.js
+++ public/Javascript/jQuery.js
@@ -2,15 +2,47 @@
 
 $(document).ready(() => {
 
-const maxwidth = $(html).css('max-width');
-const $jumbotron = $('.jumbotron');
+  const screenWidth = $(window).width();
+  const screenHeight = $(window).height();
 
-$jumbotron.on('click', event => {
-  $jumbotron.addClass('disabled');
-});
+  const carouselControls = $('.my-skills-container');
+  const carouselInner = $('.skill-category-container');
+  const carouselItem = $('.skill-category');
+  const carouselArrows = $('.carousel-arrows');
+  const carouselIndicators = $('.carousel-indicators');
 
-// if (maxwidth === '600px') {
-//   $jumbotron.addClass('')
-// }
+  const bootstrapCard = $('.my-work-container');
+  const bootstrapcardImage = $('.my-work-container img');
+  const bootstrapCardBody = $('.my-work-description');
+  const bootstrapCardTitle = $('.my-work-description h2');
+  const bootstrapCardText = $('.my-work-description p');
+
+  if (screenWidth <= '620' && screenWidth < screenHeight) {
+    carouselControls.addClass('carousel slide');
+    carouselInner.addClass('carousel-inner');
+    carouselItem.addClass('carousel-item');
+    carouselArrows.show();
+    carouselIndicators.show();
+
+    bootstrapCard.addClass('card');
+    bootstrapCard.removeClass('my-second-work');
+    bootstrapcardImage.addClass('card-img-top');
+    bootstrapCardBody.addClass('card-body');
+    bootstrapCardTitle.addClass('card-title');
+    bootstrapCardText.addClass('card-text');
+  } else {
+    carouselControls.removeClass('carousel slide');
+    carouselInner.removeClass('carousel-inner');
+    carouselItem.removeClass('carousel-item');
+    carouselArrows.hide();
+    carouselIndicators.hide();
+    bootstrapCard.removeClass('card');
+
+    bootstrapCard.removeClass('card');
+    bootstrapcardImage.removeClass('card-img-top');
+    bootstrapCardBody.removeClass('card-body');
+    bootstrapCardTitle.removeClass('card-title');
+    bootstrapCardText.removeClass('card-text');
+  }
 
 });
